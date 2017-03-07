d7965c09fcddf9938997ea66ee0dc7bd0ab51a17
Théo Morales <theo.morales.fr@gmail.com>
2017-03-07 17:26:13 +0100

    Admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->isAdmin())
+            return $next($request);
+        else
+            return back()->with(302, 'You are not an administrator.');
+    }
+}
