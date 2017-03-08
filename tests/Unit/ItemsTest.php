537b7ef5e78857d4b8917b0e686947b683a50d23
Theo Morales <theo.morales.fr@gmail.com>
2017-03-08 16:34:30 +0100

    First Unit Test

---
+++ tests/Unit/ItemsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Item;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ItemsTest extends TestCase
+{
+	use DatabaseMigrations;
+	
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateItem()
+    {
+    	$item = Item::create([
+    		'name' => 'Pizza royale',
+    		'ingredients' => 'Pepperoni, cheese, tomato, oregano',
+    		'price' => 75
+    	]);
+
+    	$this->assertDatabaseHas('items', [
+	        'name' => 'Pizza royale',
+    		'ingredients' => 'Pepperoni, cheese, tomato, oregano',
+    		'price' => 75
+	    ]);
+    }
+}
